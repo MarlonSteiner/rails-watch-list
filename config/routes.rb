e6e4f8c2018f953974819f12bccd2202d627b6f0
Marlon Steiner <marlon.steiner1999@yahoo.com>
2025-07-10 15:46:00 +0200

    added seed

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root to: "pages#home"
-  resources :railswatchlist
+  #root to: "pages#home"
+  resources :lists do
+    resources :bookmarks
+  end
 end
